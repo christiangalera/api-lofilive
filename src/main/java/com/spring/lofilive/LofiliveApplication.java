1e9afa069f7b8cbf0877f110a518f06968f9d35f
Christian <Chrisgalera1555@gmail.com>
2022-07-27 09:35:48 -0300

    first commit

---
+++ src/main/java/com/spring/lofilive/LofiliveApplication.java
@@ -0,0 +1,13 @@
+package com.spring.lofilive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LofiliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LofiliveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/lofilive/LofiliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.lofilive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LofiliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
